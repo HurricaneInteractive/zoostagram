81c2ca6a51bf58bdbd6ad87055da62274f203d1b
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-09 16:22:53 +1000

    Added firebase config file

---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+var config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID
+}
+
+firebase.initializeApp(config)
+
+export default firebase
