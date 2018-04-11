9d6cabf373a00c56e2f767ab48787b15efd19226
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-11 11:04:19 +1000

    Placeholder Capture and Entry Components

---
+++ src/components/Journey/Capture.js
@@ -0,0 +1,15 @@
+/**
+ * Placeholder Capture Component
+ * 
+ * Currently being used by AJ to test image capturing
+ */
+
+import React from 'react'
+
+const Capture = () => {
+    return (
+        <h1>Capture</h1>
+    )
+}
+
+export default Capture
