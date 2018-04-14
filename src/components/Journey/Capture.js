a21171acfba64e0f293d575e000aa331df32ae2c
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-14 23:28:23 +1000

    Added basic Capture Component functionality / placeholder capture styling

--- src/components/Journey/Capture.js
+++ src/components/Journey/Capture.js
@@ -4,12 +4,55 @@
  * Currently being used by AJ to test image capturing
  */
 
-import React from 'react'
+import React, { Component } from 'react'
 
-const Capture = () => {
-    return (
-        <h1>Capture</h1>
-    )
+class Capture extends Component {
+    constructor() {
+        super()
+        this.state = {
+            stream: null
+        }
+    }
+
+    componentWillUnmount() {
+        this.state.stream.getTracks()[0].stop();
+    }
+
+    componentDidMount() {
+        const mediaConstraints = {
+            audio: false,
+            video: {
+                facingMode: "user"
+            }
+        }
+
+        navigator.mediaDevices.getUserMedia(mediaConstraints)
+            .then((mediaStream) => {
+                let video = document.getElementById('main-camera')
+                video.srcObject = mediaStream;
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                }
+                this.setState({
+                    stream: mediaStream
+                })
+            })
+            .catch((err) => {
+                console.log(err.name + ": " + err.message);
+            })
+    }
+
+    render() {
+        return (
+            <div className="camera-feed">
+                <video id="main-camera" />
+                <canvas id="image-copy" />
+                <div className="camera-controls">
+                    <a className="take-photo">Take Photo</a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Capture
