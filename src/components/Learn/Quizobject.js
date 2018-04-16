94a5fe051a9c2d30fc7466163ec4046f5d8a959c
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-04-16 11:14:55 +1000

    added template code to QuizObject page

---
+++ src/components/Learn/Quizobject.js
@@ -0,0 +1,20 @@
+/**
+ * Placeholder Capture Component
+ * 
+ * Currently being used by AJ to test image capturing
+ */
+
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+const QuizObject = () => {
+    return (
+        <div>
+            <h1>Learn</h1>
+            <Link to="/">link</Link>
+        </div>
+        
+    )
+}
+
+export default QuizObject
