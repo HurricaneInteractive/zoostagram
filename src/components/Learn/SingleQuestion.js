fd4b1983447c6d2fdc140221a8be120c8366796f
Will <u3142921@uni.canberra.edu.au>
2018-04-22 16:22:55 +1000

    adding quiz shapes

--- src/components/Learn/SingleQuestion.js
+++ src/components/Learn/SingleQuestion.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SingleQuestion = () => {
     return (
-        <div>
+        <div id="quiz-question">
             <h2>{this.state.allLearnData.question}</h2>
             <ul>
                 <li>{this.state.allLearnData.options[0]}</li>
