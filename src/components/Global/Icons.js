1a2a3d7759f41ec1fed66fdbeca28ba90e4c696c
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-23 16:48:16 +1000

    Created File for SVG components

---
+++ src/components/Global/Icons.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+/**
+ * SVG Code for the three dots
+ * 
+ * @returns DOM
+ */
+export const SettingsIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 4">
+            <path d="M6,10a2,2,0,1,0,2,2A2.006,2.006,0,0,0,6,10Zm12,0a2,2,0,1,0,2,2A2.006,2.006,0,0,0,18,10Zm-6,0a2,2,0,1,0,2,2A2.006,2.006,0,0,0,12,10Z" transform="translate(-4 -10)"/>
+        </svg>
+    )
+}
+
+/**
+ * SVG Code for the default user profile
+ * 
+ * @returns DOM
+ */
+export const DefaultUserImage = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+            <path d="M42,2A40,40,0,1,0,82,42,40.015,40.015,0,0,0,42,2Zm0,12A12,12,0,1,1,30,26,11.984,11.984,0,0,1,42,14Zm0,56.8A28.8,28.8,0,0,1,18,57.92c.12-7.96,16-12.32,24-12.32,7.96,0,23.88,4.36,24,12.32A28.8,28.8,0,0,1,42,70.8Z" transform="translate(-2 -2)"/>
+        </svg>
+    )
+}
