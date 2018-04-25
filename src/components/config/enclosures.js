dfc82ef6cdaafbb25969274d6c977b95e4ef03e2
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-25 15:13:50 +1000

    Added map coordinates for the enclosures

--- src/components/config/enclosures.js
+++ src/components/config/enclosures.js
@@ -1,3 +1,37 @@
 export default {
-    enclosures: ["lions", "meerkats", "goats", "monkeys", "tiger", "hippo", "koala", "kangaroo"]
+    enclosures: ["lions", "meerkats", "goats", "monkeys", "tiger", "hippo", "koala", "kangaroo"],
+    coordinates: {
+        lions: {
+            x: 75,
+            y: 421
+        },
+        meerkats: {
+            x: 146,
+            y: 218
+        },
+        goats: {
+            x: 283,
+            y: 80
+        },
+        monkeys: {
+            x: 91,
+            y: 91
+        },
+        tiger: {
+            x: 361,
+            y: 334
+        },
+        hippo: {
+            x: 423,
+            y: 206
+        },
+        koala: {
+            x: 446,
+            y: 33
+        },
+        kangaroo: {
+            x: 253,
+            y: 459
+        }
+    }
 }
