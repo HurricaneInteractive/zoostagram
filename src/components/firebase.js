a71d16983abc914363bbe9020f695beafb5d2e3c
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-28 11:41:54 +1000

    Removed dotenv / updated firebase config method

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,15 +1,13 @@
 import * as firebase from 'firebase'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import FirebaseConfig from './config/firebase-config';
 
 var config = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID
+    apiKey: FirebaseConfig.APIKEY,
+    authDomain: FirebaseConfig.AUTHDOMAIN,
+    databaseURL: FirebaseConfig.DATABASEURL,
+    projectId: FirebaseConfig.PROJECTID,
+    storageBucket: FirebaseConfig.STORAGEBUCKET,
+    messagingSenderId: FirebaseConfig.MESSAGINGSENDERID
 }
 
 firebase.initializeApp(config)
