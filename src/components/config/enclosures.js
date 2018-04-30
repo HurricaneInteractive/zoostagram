b55d0e69f47fda3b17255b7189e344d181df8113
Adro.Codes <hurricane.interactive@gmail.com>
2018-04-30 20:52:35 +1000

    Added map animal images / all enclosure configuration

--- src/components/config/enclosures.js
+++ src/components/config/enclosures.js
@@ -1,37 +1,150 @@
+import agouti from '../../images/map/animal_agouti.svg'
+import capuchin from '../../images/map/animal_capuchin.svg'
+import cheetah from '../../images/map/animal_cheetah.svg'
+import deer from '../../images/map/animal_deer.svg'
+import eland from '../../images/map/animal_eland.svg'
+import giraffe from '../../images/map/animal_giraffe.svg'
+import hyena from '../../images/map/animal_hyena.svg'
+import koala from '../../images/map/animal_koala.svg'
+import lion from '../../images/map/animal_lion.svg'
+import meerkat from '../../images/map/animal_meerkat.svg'
+import monkeys from '../../images/map/animal_monkeys.svg'
+import owl from '../../images/map/animal_owl.svg'
+import red_panda from '../../images/map/animal_red-panda.svg'
+import rhino from '../../images/map/animal_rhino.svg'
+import snow_leopard from '../../images/map/animal_snow-leopard.svg'
+import tiger from '../../images/map/animal_tiger.svg'
+import tree_kangaroo from '../../images/map/animal_tree-kangaroo.svg'
+import wombat from '../../images/map/animal_wombat.svg'
+
 export default {
-    enclosures: ["lions", "meerkats", "goats", "monkeys", "tiger", "hippo", "koala", "kangaroo"],
+    enclosures: ["hyena", "monkeys", "snow_leopard", "capuchin", "lion", "koala", "tiger", "owl", "tree_kangaroo", "meerkat", "deer", "wombat", "agouti", "cheetah", "red_panda", "giraffe", "eland", "rhino"],
     coordinates: {
-        lions: {
-            x: 75,
-            y: 421
-        },
-        meerkats: {
-            x: 146,
-            y: 218
-        },
-        goats: {
-            x: 283,
-            y: 80
+        hyena: {
+            x: 180,
+            y: 77,
+            width: 58,
+            height: 58,
+            img: hyena
         },
         monkeys: {
-            x: 91,
-            y: 91
+            x: 135,
+            y: 133,
+            width: 58,
+            height: 58,
+            img: monkeys
         },
-        tiger: {
-            x: 361,
-            y: 334
+        snow_leopard: {
+            x: 235,
+            y: 139,
+            width: 58,
+            height: 58,
+            img: snow_leopard
         },
-        hippo: {
-            x: 423,
-            y: 206
+        capuchin: {
+            x: 169,
+            y: 195,
+            width: 58,
+            height: 58,
+            img: capuchin
+        },
+        lion: {
+            x: 275,
+            y: 234,
+            width: 58,
+            height: 58,
+            img: lion
         },
         koala: {
-            x: 446,
-            y: 33
+            x: 196,
+            y: 271,
+            width: 58,
+            height: 58,
+            img: koala
         },
-        kangaroo: {
-            x: 253,
-            y: 459
+        tiger: {
+            x: 83,
+            y: 266,
+            width: 58,
+            height: 58,
+            img: tiger
+        },
+        owl: {
+            x: 58,
+            y: 338,
+            width: 58,
+            height: 58,
+            img: owl
+        },
+        tree_kangaroo: {
+            x: 154,
+            y: 346,
+            width: 58,
+            height: 58,
+            img: tree_kangaroo
+        },
+        meerkat: {
+            x: 223,
+            y: 333,
+            width: 58,
+            height: 58,
+            img: meerkat
+        },
+        deer: {
+            x: 294,
+            y: 331,
+            width: 58,
+            height: 58,
+            img: deer
+        },
+        wombat: {
+            x: 94,
+            y: 405,
+            width: 59,
+            height: 60,
+            img: wombat
+        },
+        agouti: {
+            x: 274,
+            y: 400,
+            width: 58,
+            height: 58,
+            img: agouti
+        },
+        cheetah: {
+            x: 206,
+            y: 456,
+            width: 106,
+            height: 58,
+            img: cheetah
+        },
+        red_panda: {
+            x: 301,
+            y: 486,
+            width: 58,
+            height: 58,
+            img: red_panda
+        },
+        giraffe: {
+            x: 161,
+            y: 570,
+            width: 58,
+            height: 99,
+            img: giraffe
+        },
+        eland: {
+            x: 206,
+            y: 657,
+            width: 58,
+            height: 58,
+            img: eland
+        },
+        rhino: {
+            x: 141,
+            y: 717,
+            width: 58,
+            height: 58,
+            img: rhino
         }
     }
 }
