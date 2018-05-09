8b6771e89bd726acac03ffd2c6cbf91656afbcac
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-05-09 13:29:31 +1000

    badge config created, achievement scss added, badges now in view
    badge config created
    achievement scss added
    badges now in view (background image)
    renamed badge images (and put in badge folder)

--- src/components/Global/Achievements.js
+++ src/components/Global/Achievements.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import firebase from '../firebase'
 
-import PageTitle from './PageTitle'
+import PageTitle from './PageTitle';
+import Badges from '../config/badges';
 
 export default class Achievements extends Component {
     constructor(props) {
@@ -25,41 +26,36 @@ export default class Achievements extends Component {
             _this.setState({
                 allUserData: snap.val()
             })
-            console.log("inside componentWillMount Function (after setState): ");
-            console.log(this.state.allUserData);
         })
     }
 
     badgeView() {
         let { allUserData } = this.state;
-        // console.log( this.state );
-        console.log("inside badgeView Function: ");
-        console.log( allUserData );
         let badgesArray = allUserData.badges_earned;
         let badgeDataArray = Object.keys(badgesArray);
         console.log(badgeDataArray);
         let allBadges = badgeDataArray.map((item, key) => {
+            let badgeName = item.replace(/[_-]/g, " ");
             return (
-                <div>
-                    <h1>{item}</h1>
+                <div className="badge-container" key={key}>
+                    {/* <Link to="/"> link to individual badge view */}
+                        <div className="badge-img" style={{ backgroundImage: `url(${Badges.badgeInfo[item].img})` }} />
+                        <h3>{badgeName}</h3>
+                    {/* </Link> */}
                 </div>
             )
         })
         return allBadges;
-        // return (
-        //     <div>Achievement Page - wohooo!!!!!</div>
-        // )
     }
 
     render() {
         // eslint-disable-next-line
         const { user, allUserData } = this.state;
-        // console.log(userDetails);
 
         return (
             <div className="profile-page">
                 <PageTitle title="Achievements" back={() => this.props.routerProps.history.goBack()} />
-                <div className="user-achievments">
+                <div className="user-achievements">
                     { allUserData === null ? ('') : ( this.badgeView() ) }
                 </div>
             </div>
